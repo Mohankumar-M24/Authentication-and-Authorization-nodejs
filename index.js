2f8282c097bf8158875bd757ec2633f965570190
Mohankumar-M24 <1726k0146@gmail.com>
2025-06-12 11:33:02 +0530

    done

--- index.js
+++ index.js
@@ -26,3 +26,8 @@ app.use('/api/user', userRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to back Auth API');
+});
+
